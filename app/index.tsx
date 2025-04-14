1695439dedfddbfa390e1614da750e3d80798d21
jctech111 <jctech333@gmail.com>
2025-04-14 22:40:02 +0100

    working on the text on the video page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'expo-router';
 import { useVideoPlayer, VideoView, VideoSource } from 'expo-video';
 import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
-
+import { defaultStyles } from '@/constants/Styles';
 const bigBuckBunnySource: VideoSource =
     'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
 
@@ -23,15 +23,27 @@ export default function PreloadingVideoPlayerScreen() {
             />
 
             {/* Overlay Content */}
-            <View style={styles.overlay}>
-                <Text style={styles.header}>Ready to change the way you manage money?</Text>
-                <View style={styles.buttons}>
-                    <Link href={'./login'} asChild>
-                        <TouchableOpacity style={styles.loginButton}>
-                            <Text style={styles.loginText}>Login</Text>
-                        </TouchableOpacity>
-                    </Link>
-                </View>
+            <View style={{ marginTop: 80, padding: 20 }}>
+                <Text style={styles.header}>Ready to change the way you money?</Text>
+            </View>
+
+            <View style={styles.buttons}>
+                <Link
+                    href={'/login'}
+                    style={[defaultStyles.pillButton, { flex: 1, backgroundColor: Colors.dark }]}
+                    asChild>
+                    <TouchableOpacity>
+                        <Text style={{ color: 'white', fontSize: 22, fontWeight: '500' }}>Log in</Text>
+                    </TouchableOpacity>
+                </Link>
+                <Link
+                    href={'/signup'}
+                    style={[defaultStyles.pillButton, { flex: 1, backgroundColor: '#fff' }]}
+                    asChild>
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 22, fontWeight: '500' }}>Sign up</Text>
+                    </TouchableOpacity>
+                </Link>
             </View>
         </View>
     );
@@ -52,27 +64,16 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
     },
     header: {
-        fontSize: 32,
+        fontSize: 36,
         fontWeight: '900',
         textTransform: 'uppercase',
         color: 'white',
-        textAlign: 'center',
-        marginBottom: 30,
     },
     buttons: {
         flexDirection: 'row',
         justifyContent: 'center',
         gap: 20,
-    },
-    loginButton: {
-        backgroundColor: '#1E90FF',
-        paddingVertical: 12,
-        paddingHorizontal: 30,
-        borderRadius: 10,
-    },
-    loginText: {
-        color: 'white',
-        fontSize: 18,
-        fontWeight: 'bold',
+        marginBottom: 60,
+        paddingHorizontal: 20,
     },
 });
